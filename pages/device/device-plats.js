da9ef71cbc25878a9261f07c76ea1472ee7d916c
hujuran <hujuran@outlook.com>
2021-12-11 19:49:51 +0800

    1

--- pages/device/device-plats.js
+++ pages/device/device-plats.js
@@ -2,50 +2,50 @@ import myutils from "../../tools/myutils";
 import platListItems from "../plat/plat-list-item";
 
 const devicePlatList = {
-  "type": "page",
-  "title": "装备->平台列表",
-  "remark": null,
-  "name": "page-demo",
-  // "toolbar": [{
-  //   "type": "button",
-  //   "actionType": "link",
-  //   "link": "/crud/url/url-add",
-  //   "label": "新增",
-  //   "primary": true
-  // }],
-  "body": [
-      {
+    "type": "page",
+    "title": "装备->平台列表",
+    "remark": null,
+    "name": "page-demo",
+    // "toolbar": [{
+    //   "type": "button",
+    //   "actionType": "link",
+    //   "link": "/crud/url/url-add",
+    //   "label": "新增",
+    //   "primary": true
+    // }],
+    "body": [{
 
-      },
-      {
-    "type": "crud",
-    "name": "sample",
-    "perPage": 10,
-    "data": {
-      "page": 1
-    },
-    // "api": {
-    //   "method": "get",
-    //   "url": "/api/app?limit=${page}"
-    // },
-    api: {
-      method: 'get',
-      url: '/api/plat/0.1/device-id/${params.id}',
-    //   requestAdaptor: myutils.requestAdaptor,
-      adaptor: myutils.listResponseAdapter
-    },
+        },
+        {
+            "type": "crud",
+            "name": "sample",
+            "perPage": 10,
+            "data": {
+                "page": 1
+            },
+            // "api": {
+            //   "method": "get",
+            //   "url": "/api/app?limit=${page}"
+            // },
+            api: {
+                method: 'get',
+                url: '/api/plat/0.1/device-id/${params.id}',
+                //   requestAdaptor: myutils.requestAdaptor,
+                adaptor: myutils.listResponseAdapter
+            },
 
-    "columns":platListItems,
-    "affixHeader": true,
-    "columnsTogglable": "auto",
-    "placeholder": "暂无数据",
-    "tableClassName": "table-db table-striped",
-    "headerClassName": "crud-table-header",
-    "footerClassName": "crud-table-footer",
-    "toolbarClassName": "crud-table-toolbar",
-    "combineNum": 0,
-    "bodyClassName": "panel-default"
-  }]
+            "columns": platListItems,
+            "affixHeader": true,
+            "columnsTogglable": "auto",
+            "placeholder": "暂无数据",
+            "tableClassName": "table-db table-striped",
+            "headerClassName": "crud-table-header",
+            "footerClassName": "crud-table-footer",
+            "toolbarClassName": "crud-table-toolbar",
+            "combineNum": 0,
+            "bodyClassName": "panel-default"
+        }
+    ]
 }
 
 export default devicePlatList;
