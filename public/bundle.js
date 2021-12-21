6a4e9cb6bf9a49582f5444aec955941d5b7cb612
hujuran <hujuran@outlook.com>
2021-12-21 02:43:49 +0000

    微调

--- public/bundle.js
+++ public/bundle.js
@@ -618,41 +618,56 @@
             url: "/api/device/0.1/${params.id}",
             adaptor: myutils.platItemResponseAdapter
         },
-        "toolbar": [{
-            "type": "button",
-            "actionType": "button",
-            "label": "返回列表",
-            onClick: () => {
-                window.history.back();
-            }
-        }],
-        "body": {
-            "type": "panel",
-            "body": [{
-                    "type": "container",
-                    "body": "<div style='font-size: 18px;padding: 4px;font-family:Simsun;text-align:center' class='plat-title'>${country}</div>"
-                },
-                {
-                    "type": "page",
-                    "body": "<div style='background-color:#E4D9CA;padding:4px;font-size:16px;color:#425EAF;'>1.<span class='test1'>简况</span></div>"
-                },
-                {
-                    "type": "page",
-                    "body": {
-                        "type": "each",
-                        "name": "profileKvList",
-                        "items": {
-                            "type": "tpl",
-                            "tpl": "<div style='font-weight: 500' ><span class='sub-label' style=' color:#425EAF;'><span class='label-text' style='display:inline-block;min-width:56px;text-align:justify;'> <%= data.key %></span>: </span><span style='color:#595959;font-family:Simsun'> <%= data.value %></span></div> "
-                        }
+        // "toolbar": [{
+        //     "type": "button",
+        //     "actionType": "button",
+        //     "label": "返回列表",
+        //     onClick: () => {
+        //         window.history.back();
+        //     }
+        // }],
+        "body":[
+            {
+                "type": "container",
+                "className":"my-2",
+                "body": {
+                    "type": "button",
+                    "actionType": "button",
+                    "label": "返回",
+                    
+                    onClick: () => {
+                        window.history.back();
                     }
+                }
+            },
+         {
+            "type": "panel",
+            "body": [
+                // {
+                //     "type": "container",
+                //     "body": "<div style='font-size: 18px;padding: 4px;font-family:Simsun;text-align:center' class='plat-title'>${country}</div>"
+                // },
+                // {
+                //     "type": "page",
+                //     "body": "<div style='background-color:#E4D9CA;padding:4px;font-size:16px;color:#425EAF;'>1.<span class='test1'>简况</span></div>"
+                // },
+                // {
+                //     "type": "page",
+                //     "body": {
+                //         "type": "each",
+                //         "name": "profileKvList",
+                //         "items": {
+                //             "type": "tpl",
+                //             "tpl": "<div style='font-weight: 500' ><span class='sub-label' style=' color:#425EAF;'><span class='label-text' style='display:inline-block;min-width:56px;text-align:justify;'> <%= data.key %></span>: </span><span style='color:#595959;font-family:Simsun'> <%= data.value %></span></div> "
+                //         }
+                //     }
+                // },
+                {
+                    "type": "container",
+                    "body": "<div style='background-color:#E4D9CA;padding:4px;font-size:16px;color:#425EAF;'><span class='test1'>基本性能参数</span></div>"
                 },
                 {
-                    "type": "page",
-                    "body": "<div style='background-color:#E4D9CA;padding:4px;font-size:16px;color:#425EAF;'>2.<span class='test1'>基本性能参数</span></div>"
-                },
-                {
-                    "type": "page",
+                    "type": "container",
                     "body": {
                         "type": "each",
                         "name": "techKvList",
@@ -681,7 +696,7 @@
                 // }
 
             ]
-        }
+        }]
     };
 
     const platFormItems = [
