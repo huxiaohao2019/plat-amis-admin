54df3af8432d7b8fe99234c5dec42fa5f17be4f3
hujuran <hujuran@outlook.com>
2021-12-26 13:41:18 +0800

    1

--- pages/plat/plat-view.js
+++ pages/plat/plat-view.js
@@ -1,4 +1,3 @@
-
 import myutils from "../../tools/myutils";
 
 // let 
@@ -22,13 +21,13 @@ let detailView1 = {
     "type": "page",
     "data": {
         "arr": [{
-            "a": "收入",
-            "b": 199
-        },
-        {
-            "a": "支出",
-            "b": 299
-        }
+                "a": "收入",
+                "b": 199
+            },
+            {
+                "a": "支出",
+                "b": 299
+            }
         ]
     },
     "body": [{
@@ -56,93 +55,93 @@ let detailView = {
     "body": {
         "type": "service",
         "body": [{
-            "type": "container",
-            "body": "<div style='font-size: 18px;padding: 4px;font-family:Simsun;text-align:center' class='plat-title'>${name}</div>"
-        },
-        {
-            "type": "page",
-            "data": {
-                "html": "<img src=${imgSrc}>"
+                "type": "container",
+                "body": "<div style='font-size: 18px;padding: 4px;font-family:Simsun;text-align:center' class='plat-title'>${name}</div>"
             },
-            "body": {
-                "type": "tpl",
-                //   "tpl": "${html|raw}"
-                "tpl": "<div style='text-align:center'><img width='320' src=${imgSrc}></div>"
-            }
-        },
-        {
-            "type": "page",
-            "body": "<div style='background-color:#E4D9CA;padding:4px;font-size:16px;color:#425EAF;'>1.<span class='test1'>通用属性表</span></div>"
-        },
-        {
-            "type": "page",
-            "body": {
-                "type": "property",
-                "items": [{
-                    "label": "国家/地区",
-                    "content": "${country}"
+            {
+                "type": "page",
+                "data": {
+                    "html": "<img src=${imgSrc}>"
                 },
-                {
-                    "label": "科目",
-                    "content": "${category}"
-                },
-                {
-                    "label": "时间",
-                    "content": "${time|date:LLL:x}"
-                }
-                ]
-            }
-        },
-
-
-
-        {
-            "type": "page",
-            "body": "<div style='background-color:#E4D9CA;padding:4px;font-size:16px;color:#425EAF;'>1.<span class='test1'>简况</span></div>"
-        },
-        {
-            "type": "page",
-            "body": {
-                "type": "each",
-                "name": "profileKvList",
-                "items": {
+                "body": {
                     "type": "tpl",
-                    "tpl": "<div style='font-weight: 500' ><span class='sub-label' style=' color:#425EAF;'><span class='label-text' style='display:inline-block;min-width:56px;text-align:justify;'> <%= data.key %></span>: </span><span style='color:#595959;font-family:Simsun'> <%= data.value %></span></div> "
+                    //   "tpl": "${html|raw}"
+                    "tpl": "<div style='text-align:center'><img width='320' src=${imgSrc}></div>"
                 }
-            }
-        },
-        {
-            "type": "page",
-            "body": "<div style='background-color:#E4D9CA;padding:4px;font-size:16px;color:#425EAF;'>2.<span class='test1'>基本性能参数2</span></div>"
-        },
-        {
-            "type": "page",
-            "body": {
-                "type": "each",
-                "name": "techKvList",
-                "items": {
-                    "type": "tpl",
-                    "tpl": "<div style='font-weight: 500' ><span class='sub-label' style=' color:#425EAF;'><span class='label-text' style='display:inline-block;min-width:56px;text-align:justify;'> <%= data.key %></span>: </span><span style='color:#595959;font-family:Simsun'> <%= data.value %></span></div> "
+            },
+            {
+                "type": "page",
+                "body": "<div style='background-color:#E4D9CA;padding:4px;font-size:16px;color:#425EAF;'>1.<span class='test1'>通用属性表</span></div>"
+            },
+            {
+                "type": "page",
+                "body": {
+                    "type": "property",
+                    "items": [{
+                            "label": "国家/地区",
+                            "content": "${country}"
+                        },
+                        {
+                            "label": "科目",
+                            "content": "${category}"
+                        },
+                        {
+                            "label": "时间",
+                            "content": "${time|date:LLL:x}"
+                        }
+                    ]
                 }
-            }
-        },
+            },
 
-        {
-            "type": "page",
-            "body": "<div style='background-color:#E4D9CA;padding:4px;font-size:16px;color:#425EAF;'>3.<span class='test1'>主要武器</span></div>"
-        },
 
-        {
-            "type": "page",
-            "body": {
-                "type": "each",
-                "name": "weaponsKvList",
-                "items": {
-                    "type": "tpl",
-                    "tpl": "<div style='font-weight: 500' ><span class='sub-label' style=' color:#425EAF;'><span class='label-text' style='display:inline-block;min-width:56px;text-align:justify;'> <%= data.key %></span>: </span><span style='color:#595959;font-family:Simsun'> <%= data.value %></span></div> "
+
+            {
+                "type": "page",
+                "body": "<div style='background-color:#E4D9CA;padding:4px;font-size:16px;color:#425EAF;'>1.<span class='test1'>简况</span></div>"
+            },
+            {
+                "type": "page",
+                "body": {
+                    "type": "each",
+                    "name": "profileKvList",
+                    "items": {
+                        "type": "tpl",
+                        "tpl": "<div style='font-weight: 500' ><span class='sub-label' style=' color:#425EAF;'><span class='label-text' style='display:inline-block;min-width:56px;text-align:justify;'> <%= data.key %></span>: </span><span style='color:#595959;font-family:Simsun'> <%= data.value %></span></div> "
+                    }
+                }
+            },
+            {
+                "type": "page",
+                "body": "<div style='background-color:#E4D9CA;padding:4px;font-size:16px;color:#425EAF;'>2.<span class='test1'>基本性能参数2</span></div>"
+            },
+            {
+                "type": "page",
+                "body": {
+                    "type": "each",
+                    "name": "techKvList",
+                    "items": {
+                        "type": "tpl",
+                        "tpl": "<div style='font-weight: 500' ><span class='sub-label' style=' color:#425EAF;'><span class='label-text' style='display:inline-block;min-width:56px;text-align:justify;'> <%= data.key %></span>: </span><span style='color:#595959;font-family:Simsun'> <%= data.value %></span></div> "
+                    }
+                }
+            },
+
+            {
+                "type": "page",
+                "body": "<div style='background-color:#E4D9CA;padding:4px;font-size:16px;color:#425EAF;'>3.<span class='test1'>主要武器</span></div>"
+            },
+
+            {
+                "type": "page",
+                "body": {
+                    "type": "each",
+                    "name": "weaponsKvList",
+                    "items": {
+                        "type": "tpl",
+                        "tpl": "<div style='font-weight: 500' ><span class='sub-label' style=' color:#425EAF;'><span class='label-text' style='display:inline-block;min-width:56px;text-align:justify;'> <%= data.key %></span>: </span><span style='color:#595959;font-family:Simsun'> <%= data.value %></span></div> "
+                    }
                 }
             }
-        }
 
         ]
     }
@@ -156,89 +155,29 @@ let platView = {
         adaptor: myutils.platItemResponseAdapter
     },
     "body": [{
-        "type": "container",
-        "className": "my-2",
-        "body": [{
-            "type": "button",
-            "actionType": "button",
-            "label": "返回",
+            "type": "container",
+            "className": "my-2",
+            "body": [{
+                "type": "button",
+                "actionType": "button",
+                "label": "返回",
 
-            onClick: () => {
-                window.history.back();
-            }
-        },{
-            "type": "button",
-            "actionType": "button",
-            "label": "编辑",
-            "actionType": "link",
-            "link": "/plat/${params.id}/edit"
-        
-        }
-    ]
-    },
-    {
-        "type": "grid",
-        // "className": "b-a bg-dark lter",
-        "columns": [{
-            "md": 6,
-            "body": {
-                "type": "panel",
-                "title": "平台概况",
-                "body": detailView
-            }
+                onClick: () => {
+                    window.history.back();
+                }
+            }, {
+                "type": "button",
+                "actionType": "button",
+                "label": "编辑",
+                "actionType": "link",
+                "link": "/plat/${params.id}/edit"
+
+            }]
         },
         {
-            "md": 6,
-            "body": devices
-        }
-        ]
-    }
-    ]
-}
-
-let platView2 = {
-    "type": "page",
-    "body": [{
-        "type": "page",
-        "body": {
-            "type": "button",
-            "actionType": "button",
-            "label": "返回列表",
-            onClick: () => {
-                window.history.back();
-            }
-        }
-    },
-
-    {
-        "type": "page",
-        "body": {
-            "type": "button",
-            "actionType": "button",
-            "label": "返回列表",
-            onClick: () => {
-                window.history.back();
-            }
-        }
-    },
-
-    {
-
-        "type": "page",
-
-
-
-        "initApi": {
-            method: 'get',
-            url: "/api/plat/0.1/${params.id}",
-            adaptor: myutils.platItemResponseAdapter
-        },
-        "body": [
-
-            {
-                "type": "grid",
-                // "className": "b-a bg-dark lter",
-                "columns": [{
+            "type": "grid",
+            // "className": "b-a bg-dark lter",
+            "columns": [{
                     "md": 6,
                     "body": {
                         "type": "panel",
@@ -250,10 +189,69 @@ let platView2 = {
                     "md": 6,
                     "body": devices
                 }
-                ]
+            ]
+        }
+    ]
+}
+
+let platView2 = {
+    "type": "page",
+    "body": [{
+            "type": "page",
+            "body": {
+                "type": "button",
+                "actionType": "button",
+                "label": "返回列表",
+                onClick: () => {
+                    window.history.back();
+                }
             }
-        ]
-    }
+        },
+
+        {
+            "type": "page",
+            "body": {
+                "type": "button",
+                "actionType": "button",
+                "label": "返回列表",
+                onClick: () => {
+                    window.history.back();
+                }
+            }
+        },
+
+        {
+
+            "type": "page",
+
+
+
+            "initApi": {
+                method: 'get',
+                url: "/api/plat/0.1/${params.id}",
+                adaptor: myutils.platItemResponseAdapter
+            },
+            "body": [
+
+                {
+                    "type": "grid",
+                    // "className": "b-a bg-dark lter",
+                    "columns": [{
+                            "md": 6,
+                            "body": {
+                                "type": "panel",
+                                "title": "平台概况",
+                                "body": detailView
+                            }
+                        },
+                        {
+                            "md": 6,
+                            "body": devices
+                        }
+                    ]
+                }
+            ]
+        }
     ]
 }
 
